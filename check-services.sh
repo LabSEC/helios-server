2798fccecbaa2cee4d118dc60f08d90301ce5440
Jean Martina <jean.martina@gmail.com>
2020-07-31 11:48:47 -0300

    correçào de erros em cerimônia

--- check-services.sh
+++ check-services.sh
@@ -4,8 +4,8 @@ SERVICES=('celeryd' 'celerybeat')
 for service in ${SERVICES[@]}; do
     if ps ax | grep -v grep | grep $service > /dev/null
     then
-        #echo "$service service running, everything is fine"
-    else
+        echo "$service service running, everything is fine"
+	    else
         echo "$service is not running"
         cd /home/votacao/helios-server && \
         source venv/bin/activate
